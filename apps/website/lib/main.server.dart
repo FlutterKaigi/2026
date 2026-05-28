93754b1cb4ac647ff37a4147b79d14891c1661f0
jiyuujin <y-kitamura@wevnal.co.jp>
2026-05-28 22:47:17 +0900

    fix: script content

--- apps/website/lib/main.server.dart
+++ apps/website/lib/main.server.dart
@@ -71,16 +71,15 @@ void main() {
           src: 'https://www.googletagmanager.com/gtag/js?id=G-0FZ58E7XNG',
           async: true,
         ),
-        script([
-          RawText('''
+        script(
+          content: '''
 window.dataLayer = window.dataLayer || [];
 function gtag() {
   dataLayer.push(arguments);
 }
 gtag('js', new Date());
 gtag('config', 'G-0FZ58E7XNG');
-'''),
-          ],
+''',
         ),
       ],
       styles: [
